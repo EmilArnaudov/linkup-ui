61636fc12fbb0b552708ada42ea8abb9e205fc1a
EmilArnaudov <emil.arnaudov.ecom@gmail.com>
2024-01-09 16:56:39 +0200

    design changes

--- src/pages/app-main-page/AppMainPage.tsx
+++ src/pages/app-main-page/AppMainPage.tsx
@@ -1,5 +1,27 @@
+import { Box, Stack, useTheme } from '@mui/material';
+import { Image } from 'components';
+
 const AppMainPage = () => {
-  return <div>Welcome to the app</div>;
+  const theme = useTheme();
+
+  return (
+    <Stack
+      sx={{ backgroundColor: theme.palette.background.default }}
+      direction="column"
+    >
+      <Box marginLeft="auto" width="70%" height={600}>
+        <Image
+          src="/app-image-main.png"
+          height={1}
+          width={1}
+          alt="main image"
+          sx={{
+            width: '100%',
+          }}
+        />
+      </Box>
+    </Stack>
+  );
 };
 
 export default AppMainPage;
--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -1,14 +1,14 @@
 // import styled from '@emotion/styled';
 import { Box, Button, Stack, Typography, useTheme } from '@mui/material';
-import { Image, ImageCarousel } from 'components';
+import { Image } from 'components';
 import InformationSection from './InformationSection';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useGamesStore } from 'stores/games/GamesStore';
 import AuthModal from 'components/auth-modal/AuthModal';
 
 const LandingPage = () => {
   const theme = useTheme();
-  const { images, getAllGames } = useGamesStore((state) => ({
+  const { getAllGames } = useGamesStore((state) => ({
     getAllGames: state.getAllGames,
     images: state.imagesForMainCarousel,
   }));
@@ -19,29 +19,29 @@ const LandingPage = () => {
 
   const [modalOpen, setModalOpen] = useState(false);
 
-  const Background = () => {
-    return (
-      <Image
-        src="/background.jpg"
-        alt="hero page background"
-        width={0}
-        height={0}
-        sx={{
-          position: 'absolute',
-          zIndex: -2,
-          backgroundRepeat: 'no-repeat',
-          backgroundSize: 'cover',
-          height: 'calc(100vh - 70px)',
-          width: '100%',
-        }}
-      />
-    );
-  };
+  // const Background = () => {
+  //   return (
+  //     <Image
+  //       src="/background.jpg"
+  //       alt="hero page background"
+  //       width={0}
+  //       height={0}
+  //       sx={{
+  //         position: 'absolute',
+  //         zIndex: -2,
+  //         backgroundRepeat: 'no-repeat',
+  //         backgroundSize: 'cover',
+  //         height: 'calc(100vh - 70px)',
+  //         width: '100%',
+  //       }}
+  //     />
+  //   );
+  // };
 
-  const MemoizedCarousel = useMemo(
-    () => <ImageCarousel mt={10} images={images()} />,
-    [images()],
-  );
+  // const MemoizedCarousel = useMemo(
+  //   () => <ImageCarousel mt={10} images={images()} />,
+  //   [images()],
+  // );
 
   const handleAuthModal = () => {
     setModalOpen((prevState) => !prevState);
@@ -52,36 +52,62 @@ const LandingPage = () => {
       <Box
         sx={{
           height: 'calc(100vh - 70px)',
+          backgroundColor: theme.palette.background.default,
         }}
       >
-        <Background />
+        {/* <Background /> */}
+        <Image
+          src="/app-image-main.png"
+          height={1}
+          width={1}
+          alt="fantasy characters"
+          sx={{
+            position: 'absolute',
+            width: '65%',
+            height: '75%',
+            right: -50,
+            bottom: 100,
+          }}
+        />
         <Stack>
           <Typography
-            variant="h1"
+            variant="h2"
             color={theme.palette.primary.contrastText}
             sx={{
-              mt: '6%',
-              mx: 'auto',
+              mt: '7%',
+              ml: '6%',
               maxWidth: '70%',
             }}
           >
             Find Gaming Partners!
           </Typography>
-        </Stack>
-
-        {MemoizedCarousel}
-
-        <Stack direction="row" justifyContent="center" mt={8}>
-          <Button
-            variant="contained"
-            color="secondary"
-            size="large"
-            sx={{ p: 2, mx: 'auto', mt: 2 }}
-            onClick={handleAuthModal}
+          <Typography
+            color={theme.palette.primary.contrastText}
+            variant="h2"
+            sx={{
+              mt: 1,
+              ml: '6%',
+              maxWidth: '70%',
+              fontSize: 22,
+            }}
           >
-            Get Started
-          </Button>
+            Connect with fellow adventurers and forge your legacy!
+          </Typography>
         </Stack>
+
+        {/* {MemoizedCarousel} */}
+
+        {/* <Stack direction="row" mt={8}> */}
+        <Button
+          variant="contained"
+          color="secondary"
+          size="large"
+          sx={{ p: 2, mx: 'auto', mt: 32, ml: '20%' }}
+          onClick={handleAuthModal}
+        >
+          Get Started
+        </Button>
+        {/* </Stack> */}
       </Box>
       <InformationSection />
 
